848279be003278f3cadc7bad1c254fb64a9d2bd0
EmanAmr1 <emanamr543@gmail.com>
2024-01-25 22:10:01 +0200

    edit some styles

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,7 +1,9 @@
 import Card from 'react-bootstrap/Card';
 import '../CSS/ProductCardCss.css'
+import Button from 'react-bootstrap/Button';
+import React from 'react';
 const ProductCard = ({ productItem }) => {
-
+    
     return (
 
         <>
@@ -12,10 +14,12 @@ const ProductCard = ({ productItem }) => {
                 <Card.Img variant="top" src={productItem.image} />
                 <Card.Body>
                     <Card.Title>{productItem.title}</Card.Title>
-                   
+                    <Button variant="outline-warning" size="sm">
+          Order
+        </Button>{' '}
                 </Card.Body>
                 <Card.Footer>
-                    <small className="favbtn"><button className='details'>More Details</button>  <i className="fa-regular fa-heart"></i></small>
+                    <small className="favbtn"><button className='details'>More</button>  <i className="fa-regular fa-heart"></i></small>
                 </Card.Footer>
             </Card>
 
