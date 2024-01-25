e40e029cdc6f3b7dbd74c63ba13b90b67d5936f0
EmanAmr1 <emanamr543@gmail.com>
2024-01-25 17:22:45 +0200

    fetch products

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,10 +1,31 @@
-const ProductCard=()=>{
+import CardGroup from 'react-bootstrap/CardGroup';
+import Card from 'react-bootstrap/Card';
+const ProductCard = ({productItem}) => {
 
-return (
+    return (
 
-<></>
+        <>
 
-)
+            
+                <Card className='d-flex col-2 mx-1 my-1 '>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>{productItem.title}</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in
+                            to additional content. This content is a little bit longer.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+               
+          
+
+        </>
+
+    )
 
 
 }
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react"
+import ProductCard from "./ProductCard"
+import axios from 'axios';
 const Products = () => {
 
+    const [product, setProduct] = useState([])
 
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get(`https://api.sampleapis.com/coffee/hot`, {
+                    params: {
 
+                    },
+                });
+
+                setProduct(response.data);
+            } catch (error) {
+                console.error('Error fetching Products:', error);
+            }
+        };
+        fetchProducts();
+    }, []
+    );
     return (
 
         <>
             <div>Products</div>
-
+            <div className="container">
+                <div className="row">
+            <div className="d-flex flex-lg-row flex-md-row flex-sm-column col-lg-12 col-md-12 col-sm-12 flex-wrap justify-content-center col-12">
+            {product.map((product, index) => (<ProductCard key={index} productItem={product} />))}
+            </div>
+            </div>
+            </div>
         </>
     )
 }
