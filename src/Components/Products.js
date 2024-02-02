a61da0c2e3e2d1fc5da48a2fb01ea1b5b858ce1d
EmanAmr1 <emanamr543@gmail.com>
2024-02-02 13:53:34 +0200

    Implement coffee search functionality by name

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react"
 import ProductCard from "./ProductCard"
 import axios from 'axios';
 import '../CSS/ProductsCss.css'
+import { Form } from "react-bootstrap";
 const Products = () => {
 
     const [product, setProduct] = useState([])
+    const [search,setSearch]=useState("")
+
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -29,8 +32,17 @@ const Products = () => {
             <div className="d-flex justify-content-center my-5 ourPro" >Our Products </div>
             <div className="container-fluid">
                 <div className="row justify-content-center ">
+                    <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="me-2 ms-5"
+                        aria-label="Search"
+                        style={{width:'50%',border:'3px solid black'}}
+                        onChange={(e)=>setSearch(e.target.value)}
+                    />
                     <div className="d-flex flex-lg-row flex-md-row flex-sm-column justify-content-center  flex-wrap col-12">
-                        {product.map((product, index) => (<ProductCard key={index} productItem={product} />))}
+                        {product.filter((product)=> { return search.toLowerCase() === ''? product : product.title.toLowerCase().includes(search); })
+                        .map((product, index) => (<ProductCard key={index} productItem={product} />))}
                     </div>
                 </div>
             </div>
